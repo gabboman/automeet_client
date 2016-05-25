dbbd892390b936269d934598c803b6c3180012cb
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-05-25 19:09:51 +0200

    Posteamos datos y recibimos una respuesta básica

--- app/src/main/java/com/example/ale/carsharing_university/Registro.java
+++ app/src/main/java/com/example/ale/carsharing_university/Registro.java
@@ -2,6 +2,7 @@ package com.example.ale.carsharing_university;
 
 
 import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -21,6 +22,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,6 +31,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -81,7 +84,7 @@ public class Registro extends AppCompatActivity {
 
 
     //Metodos de gabriel para peticion json
-    private StringBuffer ApiPetition(String method,String urlString) {
+    private StringBuffer ApiPetition(String urlString) {
 
 
         StringBuffer chaine = new StringBuffer("");
@@ -89,7 +92,7 @@ public class Registro extends AppCompatActivity {
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
             connection.setRequestProperty("User-Agent", "");
-            connection.setRequestMethod(method);//connection.setRequestMethod("POST")
+            connection.setRequestMethod("GET");//connection.setRequestMethod("POST")
             connection.setDoInput(true);
             connection.connect();
 
@@ -108,11 +111,42 @@ public class Registro extends AppCompatActivity {
 
         return chaine;
     }
+
+    private StringBuffer ApiPost(String urlString ) {
+
+
+        StringBuffer chaine = new StringBuffer("");
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("User-Agent", "");
+            connection.setRequestMethod("POST");//connection.setRequestMethod("POST")
+            connection.setDoInput(true);
+            connection.connect();
+
+            InputStream inputStream = connection.getInputStream();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                chaine.append(line);
+            }
+
+        } catch (IOException e) {
+            // writing exception to log
+            e.printStackTrace();
+        }
+
+        return chaine;
+    }
+
+
     private class AsyncRequest extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String ... urls) {
 
-            return new String(ApiPetition(urls[0],urls[1]));
+            return new String(ApiPetition(urls[0]));
         }
         // onPostExecute displays the results of the AsyncTask.
         @Override
@@ -149,7 +183,12 @@ public class Registro extends AppCompatActivity {
         //Descargar archivo JSON
         //new DescargaJSON().execute();
         pueblo=(Spinner)findViewById(R.id.spinner_pueblos);
-        new AsyncRequest().execute("GET", "http://mc.hamburcraft.xyz:5000/pueblos/");
+        new AsyncRequest().execute("http://mc.hamburcraft.xyz:5000/pueblos/");
+        try {
+            new PostCommentTask().execute(new URL("http://mc.hamburcraft.xyz:5000/test/"));//android studio se queja
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         //new DownloadJSON().execute();
 
         // Acción al precionar boton de registro
@@ -279,35 +318,7 @@ public class Registro extends AppCompatActivity {
                     Log.w("automeetjson", jsonobject.toString());
                     jsonarray = jsonobject.getJSONArray("");
                     listaPueblos.add(jsonobject.optString("3"));
-                    /*for(int i=3; i<jsonarray.length(); i++){
 
-                        listaPueblos.add(jsonobject.optString(i));
-                    }*/
-                    // Nos recorremos con un iterador todos los pueblos de la API
-                    /*for(Iterator<String> iter = json.keys();iter.hasNext();){
-                        String id_pueblo = iter.next();
-
-                        // Añadimos los pueblos a la lista de pueblos
-                        listaPueblos.add(json.getString(id_pueblo));
-
-                    }*/
-                    /*// Locate the NodeList name
-                    jsonarray = jsonobject.getJSONArray("worldpopulation");
-                    for (int i = 0; i < jsonarray.length(); i++) {
-                        jsonobject = jsonarray.getJSONObject(i);
-
-                        WorldPopulation worldpop = new WorldPopulation();
-
-                        worldpop.setRank(jsonobject.optString("rank"));
-                        worldpop.setCountry(jsonobject.optString("country"));
-                        worldpop.setPopulation(jsonobject.optString("population"));
-                        worldpop.setFlag(jsonobject.optString("flag"));
-                        world.add(worldpop);
-
-                        // Populate spinner with country names
-                        worldlist.add(jsonobject.optString("country"));
-
-                    }*/
                 } catch (Exception e) {
                     //Log.e("Error", e.getMessage());
                     e.printStackTrace();
@@ -357,5 +368,73 @@ public class Registro extends AppCompatActivity {
                 });
             }
         }
+
+
+    public class PostCommentTask extends AsyncTask<URL, Void, Void> {
+
+        @Override
+        protected Void doInBackground(URL... urls) {
+            // Obtener la conexión
+            HttpURLConnection con = null;
+
+            try {
+
+                con = (HttpURLConnection)urls[0].openConnection();
+
+                String url = "http://mc.hamburcraft.xyz:5000/test/";
+                String charset = "UTF-8";  // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
+                String test1 = "value1";
+                String test2 = "value2";
+
+                String query = String.format("test1=%s&test2=%s",
+                        URLEncoder.encode(test1, charset),
+                        URLEncoder.encode(test2, charset));
+
+                // Activar método POST
+                con.setDoOutput(true);
+
+                // Tamaño previamente conocido
+                con.setFixedLengthStreamingMode(query.getBytes().length);
+
+                // Establecer application/x-www-form-urlencoded debido al formato clave-valor
+                con.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+
+                /*
+                OutputStream out = new BufferedOutputStream(con.getOutputStream());
+
+                out.write(data.getBytes());
+                out.flush();
+                out.close();*/
+                OutputStream output = con.getOutputStream();
+                output.write(query.getBytes(charset));
+                InputStream response = con.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response));
+                StringBuffer chaine = new StringBuffer("");
+                String line="";
+                while ((line = rd.readLine()) != null) {
+                    chaine.append(line);
+                }
+
+                JSONObject json = new JSONObject(chaine.toString());
+                Log.w("respuesta",json.toString());
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if(con!=null)
+                    con.disconnect();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void s) {
+            Toast.makeText(getBaseContext(), "Comentario posteado", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
 
